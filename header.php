20e04359821ac571d4719d3cde53f2decba2a888
Flyerme <gao.eison@gmail.com>
2017-09-13 18:14:19 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name:官网Em6.0默认
+Description:默认模板，简洁优雅
+Author:emlog
+Author Url:http://www.emlog.net
+*/
+if(!defined('EMLOG_ROOT')) {exit('error!');}
+require_once View::getView('module');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php echo $site_title; ?></title>
+<meta name="keywords" content="<?php echo $site_key; ?>" />
+<meta name="description" content="<?php echo $site_description; ?>" />
+<meta name="generator" content="emlog" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="alternate" type="application/rss+xml" title="RSS"  href="<?php echo BLOG_URL; ?>rss.php" />
+<link href="<?php echo TEMPLATE_URL; ?>main.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo TEMPLATE_URL; ?>css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo BLOG_URL; ?>admin/editor/plugins/code/prettify.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo TEMPLATE_URL; ?>js/jquery-1.11.0.js?v=<?php echo Option::EMLOG_VERSION; ?>"></script>
+<script src="<?php echo BLOG_URL; ?>admin/editor/plugins/code/prettify.js" type="text/javascript"></script>
+<script src="<?php echo BLOG_URL; ?>include/lib/js/common_tpl.js" type="text/javascript"></script>
+<script src="<?php echo TEMPLATE_URL; ?>js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo BLOG_URL ?>lang/<?php echo  EMLOG_LANGUAGES ?>/lang_js.js"></script>
+<?php doAction('index_head'); ?>
+</head>
+<body>
+<!--导航-->
+<?php blog_navi();?>
+
+<header class="sb-page-header">
+	<div class="container">
+		<h1><?php echo $blogname; ?></h1>
+		<p><?php echo $bloginfo; ?></p>
+	</div>
+</header>
+    
+<div class="container">
+	<div class="row">
